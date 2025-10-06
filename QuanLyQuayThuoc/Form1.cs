182eeba8d7c44dfa68aa24be0cb89dcf955fe6f9
LAPTOP-BKB06HB6\Quoc Hieu <huynhquochieu127@gmail.com>
2025-10-07 02:29:10 +0700

    them ne

--- QuanLyQuayThuoc/Form1.cs
+++ QuanLyQuayThuoc/Form1.cs
@@ -125,7 +125,7 @@ namespace QuanLyQuayThuoc
 
         private void txtName_TextChanged(object sender, EventArgs e)
         {
-            //them
+            //them ne
 
 //hieu////cccc//cccccccccccccc
 
