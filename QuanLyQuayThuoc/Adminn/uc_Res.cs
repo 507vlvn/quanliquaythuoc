ae00d9b22dd71b919adff6e5096931780cc26b5b
LAPTOP-BKB06HB6\Quoc Hieu <huynhquochieu127@gmail.com>
2025-10-10 16:53:22 +0700

    hieu add

---
+++ QuanLyQuayThuoc/Adminn/uc_Res.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuayThuoc.Adminn
+{
+    public partial class uc_Res : UserControl
+    {
+        public uc_Res()
+        {
+            InitializeComponent();
+        }
+    }
+}
